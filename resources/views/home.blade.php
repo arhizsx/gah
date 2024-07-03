b182fdad38cfa706a624a7b7be49fcd48211dbbd
arhizsx <arhizsx@gmail.com>
2024-07-03 21:02:21 +0800

    GAH

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,9 @@
             {{ __('Home') }}
         </h2>
     </x-slot>
+    <x-slot name="slot">
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div> --}}
+
     <div class="container">
         <div class="row">
             <div class="col">
@@ -48,4 +41,6 @@
             </div>
         </div>
     </div>
+
+    </x-slot>
 </x-app-layout>

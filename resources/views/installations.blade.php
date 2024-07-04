28d8e339d06e5c163d9becbfa9726fc11b4ee154
arhizsx <arhizsx@gmail.com>
2024-07-04 19:57:40 +0800

    datagrid

--- resources/views/installations.blade.php
+++ resources/views/installations.blade.php
@@ -14,19 +14,78 @@
 </x-app-layout>
 
 <!-- Modal -->
-<div class="modal fade" id="installation_details" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl modal-fullscreen-lg-down">
+<div class="modal fade" id="application_details" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-fullscreen-lg-down">
         <div class="modal-content">
             <div class="modal-header bg-primary text-white">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Installation Details</h1>
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Application Details</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="container-fluid">
+                    <div class="section-title">Personal Information</div>
+                    <div class="row mt-3 mb-3">
+                        <div class="col-3">
+                            <label for="province">Customer Name</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="province" id="province" placeholder="Customer Name">
+                        </div>
+                    </div>
+                    <div class="row mb-4 border-bottom pb-3">
+                        <div class="col-3">
+                            <label for="province">Mobile Number</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="province" id="province" placeholder="Mobile Number">
+                        </div>
+                    </div>
+                    <div class="section-title">Installation Address</div>
+                    <div class="row mt-3 mb-3">
+                        <div class="col-3">
+                            <label for="province">Province</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="province" id="province" placeholder="Province">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <label for="city">City</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="city" id="city" placeholder="City">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <label for="city">Barangay / Village</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="city" id="city" placeholder="Barangay / Village">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <label for="city">Street</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="city" id="city" placeholder="Street">
+                        </div>
+                    </div>
+                    <div class="row mb-4 border-bottom pb-3">
+                        <div class="col-3">
+                            <label for="city">House no., Floor no., Bldg</label>
+                        </div>
+                        <div class="col-9">
+                            <input class="form-control form-control-sm" type="text" value="" name="city" id="city" placeholder="House no., Floor no., Bldg">
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                {{-- <button type="button" class="btn btn-primary">Installed</button> --}}
+                <button type="button" class="btn btn-primary">Installed</button>
             </div>
         </div>
     </div>

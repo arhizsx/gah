75b2134c66ba7bcb492e1597feac8242fbc36729
arhizsx <arhizsx@gmail.com>
2024-07-04 18:13:51 +0800

    datagrid

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -24,7 +24,16 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Details
+                        </div>
+                        <div class="col-lg-6">
+                            Attachments
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>

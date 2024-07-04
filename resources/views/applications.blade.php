f75684fa62a322f218dd96a16dd8f84981ddd79a
arhizsx <arhizsx@gmail.com>
2024-07-04 14:57:59 +0800

    GAH data

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -14,6 +14,57 @@
 $(() => {
   $('#gridContainer').dxDataGrid({
     dataSource: '/supervendor/applications/data',
+    rowAlternationEnabled: true,
+    selection: {
+        mode: 'single',
+    },
+    showBorders: true,
+    columnAutoWidth: false,
+    export: {
+        enabled: true,
+    },
+    columnFixing:{
+            enabled: false
+    },
+    onContentReady: function(e){
+    },
+    searchPanel: {
+        visible: true,
+        highlightCaseSensitive: true,
+    },
+    columnChooser: {
+        enabled: true,
+        mode: "select",
+        allowSearch: true,
+    },
+    scrolling: {
+        rowRenderingMode: 'virtual',
+    },
+    paging: {
+        pageSize: 50,
+    },
+    pager: {
+        visible: true,
+        allowedPageSizes: [10, 20, 50, 'all'],
+        showPageSizeSelector: true,
+        showInfo: true,
+        showNavigationButtons: true,
+    },
+    columnsAutoWidth: true,
+    showBorders: true,
+    filterRow: {
+        visible: false,
+        applyFilter: 'auto',
+    },
+    headerFilter: {
+        visible: true,
+    },
+    onRowClick: function(e) {
+
+    },
+    allowColumnResizing: {
+        enabled: true
+    },
     columns: ['id', 'name', 'email'],
     showBorders: true,
   });

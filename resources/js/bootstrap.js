7a24f16d3b75fb91ee9db0b9537e651fbda6988c
arhizsx <arhizsx@gmail.com>
2024-07-05 08:41:30 +0800

    samsung

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,3 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-$(".location_filters").on("change", function(){
-    console.log($(this));
-});

51cf6836577abc13717cf164fa6fad83458fa53b
arhizsx <arhizsx@gmail.com>
2024-07-05 09:36:54 +0800

    Location Filters

--- resources/js/location.js
+++ resources/js/location.js
@@ -25,7 +25,7 @@ $(document).on('change','.location_filters',function(){
                     var data = JSON.parse(resp) ;
 
                     $.each(data, function(k, v){
-                        console.log( v.PROVINCE );
+                        province.append('<option value="' + v.PROVINCE +'" data-region="' + v.REGION +'">' + v.PROVINCE +'</option>')
                     });
 
                 },

2c72e4a1a2f52624839f36c213f184a277ccd4de
arhizsx <arhizsx@gmail.com>
2024-07-05 11:34:39 +0800

    Location Filters

--- resources/js/location.js
+++ resources/js/location.js
@@ -1,9 +1,9 @@
 
 $(document).on('change','.location_filters',function(){
 
-    let region = $(this).closest( $(this).data("parent") ).find("[name='region']");
-    let province = $(this).closest( $(this).data("parent") ).find("[name='province']");
-    let city = $(this).closest( $(this).data("parent") ).find("[name='city']");
+    let region = $(this).closest( $(this).data("parent") ).find("[data-filter='region']");
+    let province = $(this).closest( $(this).data("parent") ).find("[data-filter='province']");
+    let city = $(this).closest( $(this).data("parent") ).find("[data-filter='city']");
 
     console.log(region);
     console.log(province);

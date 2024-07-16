3165da23b4c1670e35e02a96014e088f60007ee2
arhizsx <arhizsx@gmail.com>
2024-07-17 00:08:01 +0800

    UTM Medium

--- resources/views/installations.blade.php
+++ resources/views/installations.blade.php
@@ -95,7 +95,7 @@
 let modal = "#installation_details";
 let datagrid = "#gridContainer";
 let datasource = '/supervendor/data/installations';
-let columns = ['campaign', 'complete_name', 'mobile_number', 'province', 'city', 'supervendor'];
+let columns = ['campaign', 'complete_name', 'mobile_number', 'province', 'city', 'vendor', 'status'];
 
 $(() => {
 

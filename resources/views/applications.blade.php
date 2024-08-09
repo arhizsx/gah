19059567384c49bc82c6e189a20888adab140096
arhizsx <arhizsx@gmail.com>
2024-08-09 21:33:56 +0800

    Laravel

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -108,11 +108,20 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                @if( \Auth::user()->company != null )
+
                 <button type="button" class="btn btn-success btn-action" data-action="application_endorsed" data-id="">Endorse to SV</button>
                 <button type="button" class="btn btn-warning btn-action" data-action="application_pending" data-id="">Pending</button>
                 <button type="button" class="btn btn-danger btn-action" data-action="application_dropped" data-id="">Drop</button>
+
+                @else
+
                 <button type="button" class="btn btn-danger btn-action" data-action="application_cancelled" data-id="">Cancelled</button>
                 <button type="button" class="btn btn-primary btn-action" data-action="application_installed" data-id="">Installed</button>
+
+                @endif
+
             </div>
         </div>
     </div>

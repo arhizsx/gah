6cadddfdfe760d95061fca7657ca525a3ea144b0
arhizsx <arhizsx@gmail.com>
2024-08-27 12:29:08 +0800

    Select 2

--- resources/views/samsung.blade.php
+++ resources/views/samsung.blade.php
@@ -23,13 +23,10 @@
             .select2-container {
                 width: 100% !important;
             }
-            .select2-selection__rendered{
+            .select2-selection__rendered, .select2-selection__arrow, .select2-selection--single{
                 height: 40px !important;
             }
 
-            .select2-selection--single{
-                height: 40px !important;
-            }
         </style>
     </head>
 

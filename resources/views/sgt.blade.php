6c446f84d26bf78be8ffffc2070512a6666c8018
arhizsx <arhizsx@gmail.com>
2024-10-09 11:31:43 +0800

    Pending Types

--- resources/views/sgt.blade.php
+++ resources/views/sgt.blade.php
@@ -156,6 +156,9 @@
                             Are you sure you want to set this to <strong><span class="new_status"></span></strong>
                         </div>
                     </div>
+                    <div id="custom_field">
+
+                    </div>
                     <div class="row mt-3 mb-3">
                         <div class="col-3">
                             <label for="confirm_complete_name">Complete Name</label>
@@ -209,6 +212,8 @@ $(document).on("click", ".btn-action", function(){
             $(document).find("#application_details").modal("hide");
 
             var new_status = "";
+            $(document).find("#custom_field").html('');
+
             switch(  $(this).data("action") ){
 
                 case "application_endorsed":
@@ -217,6 +222,23 @@ $(document).on("click", ".btn-action", function(){
 
                 case "application_pending":
                     new_status = "PENDING";
+
+                    $(document).find("#custom_field").html(
+                        '<div class="row mt-3 mb-3">' +
+                            '<div class="col-3">' +
+                                '<label for="confirm_complete_name">Pending Type</label>' +
+                            '</div>' +
+                            '<div class="col-9">' +
+                                '<select name="pending_type">' +
+                                    '<option value="">Select Pending Type</option>' +
+                                    '<option value="Pending - Customer Availability">Pending - Customer Availability</option>' +
+                                    '<option value="Pending - SV Capacity Issue">Pending - SV Capacity Issue</option>' +
+                                    '<option value="Pending -Adverse Weather">Pending -Adverse Weather</option>' +
+                                    '<option value="Pending -Customer Uncontacted">Pending -Customer Uncontacted</option>' +
+                                '</select>' +
+                            '</div>' +
+                        '</div>'
+                    )
                 break;
 
                 case "application_dropped":

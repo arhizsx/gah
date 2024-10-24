2fbd04f221aae92cbfcd73710da4675fbb383469
arhizsx <arhizsx@gmail.com>
2024-10-24 13:00:24 +0800

    Store

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -19,7 +19,7 @@ let x= "";
 let modal = "";
 let datagrid = "#gridContainer";
 let datasource = '/supervendor/data/users';
-let columns = ['campaign', 'profile', 'name', 'email'];
+let columns = ['campaign', 'profile','position', 'name', 'email'];
 
 $(() => {
 

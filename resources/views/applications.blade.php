735d0c2ee7e348ec8dca34a1fef4ed22dbbf2941
arhizsx <arhizsx@gmail.com>
2024-11-01 19:24:05 +0800

    SV activation samsung

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -226,23 +226,23 @@ function callbackAction(data){
 
     }
 
-    var show_vendor = ["ENDORSED"];
-    var vendor_btns = $(document).find(modal).find(".btn-action[data-user_mode='vendor']");
+    // var show_vendor = ["ENDORSED"];
+    // var vendor_btns = $(document).find(modal).find(".btn-action[data-user_mode='vendor']");
 
-    if( $.inArray( data.status, show_vendor ) >= 0){
-        vendor_btns.removeClass("d-none");
-    } else {
-        vendor_btns.addClass("d-none");
-    }
+    // if( $.inArray( data.status, show_vendor ) >= 0){
+    //     vendor_btns.removeClass("d-none");
+    // } else {
+    //     vendor_btns.addClass("d-none");
+    // }
 
-    var show_gt = ["ENDORSED"];
-    var gt_btns = $(document).find(modal).find(".btn-action[data-user_mode='gt']");
+    // var show_gt = ["ENDORSED"];
+    // var gt_btns = $(document).find(modal).find(".btn-action[data-user_mode='gt']");
 
-    if( $.inArray( data.status, show_gt ) >= 0){
-        gt_btns.removeClass("d-none");
-    } else {
-        gt_btns.addClass("d-none");
-    }
+    // if( $.inArray( data.status, show_gt ) >= 0){
+    //     gt_btns.removeClass("d-none");
+    // } else {
+    //     gt_btns.addClass("d-none");
+    // }
 
 }
 

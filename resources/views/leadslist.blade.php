fb0a331baecdeae3d4b1107e19dbe714b33624b0
arhizsx <arhizsx@gmail.com>
2024-11-22 15:34:06 +0800

    TM Banner

--- resources/views/leadslist.blade.php
+++ resources/views/leadslist.blade.php
@@ -19,7 +19,7 @@ let x= "";
 let modal = "";
 let datagrid = "#gridContainer";
 let datasource = '/supervendor/data/leadslist';
-let columns = ['campaign', 'complete_name', 'mobile_number', 'province', 'city', 'street', 'brgy_village', 'house_floor_bldg', 'vendor', 'SGT Name', 'status', 'Registration Date', 'Last Update', "schedule_date", "Aging",
+let columns = ['ID','campaign', 'complete_name', 'mobile_number', 'province', 'city', 'street', 'brgy_village', 'house_floor_bldg', 'vendor', 'SGT Name', 'status', 'Registration Date', 'Last Update', "schedule_date", "Aging",
     {
         dataField: "area_head_name",
         caption: "Area Head",

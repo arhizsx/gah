0f5169bb9fa4de650f5a84e30c44514c82334345
arhizsx <arhizsx@gmail.com>
2024-11-23 06:04:09 +0800

    S

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -7,7 +7,7 @@
         <meta name="x-apple-disable-message-reformatting">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="telephone=no" name="format-detection">
-        <title>Globe at Home</title>
+        <title>Globe At Home</title>
         <!--[if (mso 16)]>
         <style type="text/css">
             a {text-decoration: none;}

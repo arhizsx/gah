49418e2a29cc88c95938e9473b71158ce82a88d7
arhizsx <arhizsx@gmail.com>
2024-11-24 10:29:48 +0800

    S

--- resources/views/sgt.blade.php
+++ resources/views/sgt.blade.php
@@ -337,8 +337,6 @@ $(document).on("click", ".btn-action", function(){
         
     } else {
 
-        alert("T");
-
         var action = "";
 
         if( $(this).data("action")  == "confirm_action"){

998362ba2bf6fa8f9a7478d7d0898d583fea6177
arhizsx <arhizsx@gmail.com>
2024-12-12 07:52:14 +0800

    Failed Buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
                     <div class="col">
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title">{{ $pl->project }}</h5>
+                                <h2 class="card-title">{{ $pl->project }}</h2>
                                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="#" class="card-link">Card link</a>

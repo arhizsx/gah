5cd67204fb2222d07fbf1f451fc76f8234187fa0
arhizsx <arhizsx@gmail.com>
2024-12-12 07:50:53 +0800

    Failed Buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,15 +45,18 @@
             @if( \Auth::user()->id == 1 )
 
                 @foreach($projects_list as $pl )
-
-                <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $pl->project }}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $pl->project }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 @endforeach

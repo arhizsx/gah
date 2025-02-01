c470e09076f23823402ac3d581d0f6da301194dc
arhizsx <arhizsx@gmail.com>
2025-02-01 18:01:58 +0800

    globeprepaid

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -137,7 +137,7 @@
                 <button type="button" class="btn btn-dark btn-action" data-user_mode="gt" data-action="application_cancelled" data-confirm="yes" data-id="">Cancelled</button>
                 <button type="button" class="btn btn-primary btn-action" data-user_mode="gt" data-action="application_installed" data-confirm="yes" data-id="">Installed</button>
 
-                <button type="button" class="btn btn-primary btn-action" data-user_mode="no_vendor" data-action="application_set_vendor" data-confirm="yes" data-id="">Set SV</button>
+                <button type="button" class="btn btn-primary btn-action d-none" data-user_mode="gt" data-action="application_set_vendor" data-confirm="yes" data-id="">Set SV</button>
 
             </div>
         </div>

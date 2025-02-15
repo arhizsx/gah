82abdc230dfd06165c98c3b216cde998f6f96d35
arhizsx <arhizsx@gmail.com>
2025-02-15 12:32:40 +0800

    Updates

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -469,12 +469,16 @@ $(document).on("click", ".btn-action", function(){
 
             $(document).find(".new_status").html(new_status_text);
 
+            $(document).find(".confirm_btn").attr("data-next_action", $(this).data("id"));
             $(document).find(".confirm_btn").attr("data-next_action", $(this).data("action"));
+            
             $(document).find(".confirm_btn").attr("data-id", $(this).data("id"));
 
+            var id = $(modal).find("[name='id']").val();
             var complete_name = $(modal).find("[name='complete_name']").val();
             var mobile_number = $(modal).find("[name='mobile_number']").val();
 
+            $(document).find("#confirm_modal").find("[name='confirm_id']").val(id);
             $(document).find("#confirm_modal").find("[name='confirm_complete_name']").val(complete_name);
             $(document).find("#confirm_modal").find("[name='confirm_mobile_number']").val(mobile_number);
 

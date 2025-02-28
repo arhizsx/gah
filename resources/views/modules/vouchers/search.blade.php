bf361b602d0fa424369abf06275cb1340b635319
arhizsx <arhizsx@gmail.com>
2025-02-28 22:55:30 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -5,8 +5,8 @@
         </h2>
     </x-slot>
     <x-slot name="slot">
-        <div class="border shadow-lg p-5 bg-white rounded">
-            <input type="text" class="search_input forn-control" >
+        <div class="border shadow-lg p-3 mx-3 mt-3 mb-5 bg-white rounded">
+    
         </div>
     </x-slot>
 </x-app-layout>

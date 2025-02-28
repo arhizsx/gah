12710d7e77e0353e85cc7358672b258d7014a3fa
arhizsx <arhizsx@gmail.com>
2025-02-28 23:39:31 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -11,7 +11,7 @@
                 <input type="text" class="form-control w-100 mb-3" style="font-size: 2em;">
                 
                 <div class="d-flex justify-content-end">
-                <button class="btn btn-secondary rounded-3 px-5">Clear</button>
+                <button class="btn btn-secondary rounded-3 px-5 me-3">Clear</button>
                 <button class="btn btn-primary rounded-3 px-5">Search</button>
                 </div>
             </div>

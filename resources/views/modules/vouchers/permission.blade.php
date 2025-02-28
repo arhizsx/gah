e0f1f85a8f962a2d68974f2247450d52fa778a81
arhizsx <arhizsx@gmail.com>
2025-02-28 21:29:51 +0800

    vouchers module

--- resources/views/modules/vouchers/permission.blade.php
+++ resources/views/modules/vouchers/permission.blade.php
@@ -7,9 +7,23 @@
     <x-slot name="slot">
         <div class="border shadow-lg p-3 mx-3 mt-3 mb-5 bg-white rounded">
 
-            <H1>Permission Denied</H1>
-            <hr>
-            {{ print_r(Auth::user()) }}
+            <H1 style="font-size: 1.5em">User Details</h1>
+            <table class="table-bordered table">
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>{{ Auth::user()->email }}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>{{ Auth::user()->email }}</td>
+                    </tr>
+                    <tr>
+                        <td>IP Address</td>
+                        <td>{{ $request->ip() }}</td>
+                    </tr>
+                </tbody>
+            </table>
 
         </div>
     </x-slot>

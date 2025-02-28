b06d4baf1cd066a9a607dd76aec938359091f9db
arhizsx <arhizsx@gmail.com>
2025-02-28 22:42:28 +0800

    vouchers module

--- resources/views/modules/vouchers/layout/navigation.blade.php
+++ resources/views/modules/vouchers/layout/navigation.blade.php
@@ -14,7 +14,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('vouchers')" :active="request()->routeIs('vouchers')">
-                        {{ __('Vouchers') }}
+                        {{ __('Home') }}
                     </x-nav-link>
                     @if( $position == "admin" )
                     <x-nav-link :href="route('vouchers_management')" :active="request()->routeIs('vouchers_management')">
@@ -74,7 +74,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('vouchers')" :active="request()->routeIs('vouchers')">
-                {{ __('Vouchers') }}
+                {{ __('Home') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('vouchers_management')" :active="request()->routeIs('vouchers_management')">
                 {{ __('Management') }}

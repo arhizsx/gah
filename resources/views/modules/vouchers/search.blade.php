1863484c9048a91d6001bf6d6146c25390763f8c
arhizsx <arhizsx@gmail.com>
2025-02-28 23:24:09 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="slot">
         <div class="container d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 400px)">
             <div class="border shadow-lg bg-white p-4 rounded-4 w-100" style="max-width: 600px;">
+                <label>Search</label>
                 <input type="text" class="form-control w-100" style="font-size: 2em;">
             </div>
         </div>

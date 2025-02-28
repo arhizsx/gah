a653340e8af427fcdc7ad5fed65874a62b7288b1
arhizsx <arhizsx@gmail.com>
2025-02-28 23:04:21 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
     <x-slot name="slot">
-        <div class="border shadow-lg bg-white" style="margin-left: 30%; margin-right: 30%; padding: 30px;">
+        <div class="border shadow-lg bg-white" style="margin-left: 30%; margin-right: 30%; padding: 30px; margin-top: auto; margin-bottom: auto; border-radius: 30px;">
             <input type="text" class="form-control w-100">
         </div>
     </x-slot>

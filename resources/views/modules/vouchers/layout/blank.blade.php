9b2732b317660adf3486c3be8e532e3e30797095
arhizsx <arhizsx@gmail.com>
2025-02-28 22:08:17 +0800

    vouchers module

--- resources/views/modules/vouchers/layout/blank.blade.php
+++ resources/views/modules/vouchers/layout/blank.blade.php
@@ -76,15 +76,6 @@
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('modules.vouchers.layout.no_navigation')
         
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-8xl py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
             <!-- Page Content -->
             <main>
                 {{ $slot }}

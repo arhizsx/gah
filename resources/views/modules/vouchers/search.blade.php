e84e588beff74abd83b5e9e0a6aa1b30e10aaacd
arhizsx <arhizsx@gmail.com>
2025-02-28 23:26:48 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -1,13 +1,13 @@
 <x-vouchers-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Voucher Search') }}
+            {{ __('Search') }}
         </h2>
     </x-slot>
     <x-slot name="slot">
         <div class="container d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 500px)">
             <div class="border shadow-lg bg-white p-4 rounded-4 w-100" style="max-width: 600px;">
-                <label>Search</label>
+                <label>Mobile Number</label>
                 <input type="text" class="form-control w-100" style="font-size: 2em;">
             </div>
         </div>

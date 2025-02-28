611f9c4705ce2f7b57a2620e31de223a53f6a269
arhizsx <arhizsx@gmail.com>
2025-02-28 20:50:00 +0800

    vouchers module

--- resources/views/modules/vouchers/permission.blade.php
+++ resources/views/modules/vouchers/permission.blade.php
@@ -1,7 +1,7 @@
 <x-vouchers-blank-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Vouchers') }}
+            {{ __('Unauthorized') }}
         </h2>
     </x-slot>
     <x-slot name="slot">

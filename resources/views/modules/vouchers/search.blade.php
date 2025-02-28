49c2beb0ba856b4493cd2815f9599a61a819d05a
arhizsx <arhizsx@gmail.com>
2025-02-28 23:32:04 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -7,7 +7,7 @@
     <x-slot name="slot">
         <div class="container d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 450px)">
             <div class="border shadow-lg bg-white p-5 rounded-5 w-100" style="max-width: 600px;">
-                <label class="mb-3" >Mobile Number</label>
+                <label class="mb-3">Mobile Number / Email</label>
                 <input type="text" class="form-control w-100 mb-3" style="font-size: 2em;">
                 <button class="btn btn-primary round-3 px-5">Search</button>
             </div>

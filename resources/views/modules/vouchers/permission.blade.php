be511f038a4c26ade0c63c2f8383c1872882bcda
arhizsx <arhizsx@gmail.com>
2025-02-28 20:18:51 +0800

    vouchers module

--- resources/views/modules/vouchers/permission.blade.php
+++ resources/views/modules/vouchers/permission.blade.php
@@ -1,4 +1,4 @@
-<x-vouchers-layout>
+<x-vouchers-blank-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Vouchers') }}

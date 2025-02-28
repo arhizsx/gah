f1c0522fba9639526a58fd8bb7be4ebaeea59bbb
arhizsx <arhizsx@gmail.com>
2025-02-28 23:22:20 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -1,6 +1,6 @@
 <x-vouchers-layout>
     <x-slot name="slot">
-        <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="container d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 80px)">
             <div class="border shadow-lg bg-white p-4 rounded-4 w-100" style="max-width: 600px;">
                 <input type="text" class="form-control w-100" style="font-size: 2em;">
             </div>

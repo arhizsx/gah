91c62777528c9719d67d9857bf43a64067e14699
arhizsx <arhizsx@gmail.com>
2025-02-28 23:26:33 +0800

    vouchers module

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -1,7 +1,7 @@
 <x-vouchers-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Search') }}
+            {{ __('Voucher Search') }}
         </h2>
     </x-slot>
     <x-slot name="slot">

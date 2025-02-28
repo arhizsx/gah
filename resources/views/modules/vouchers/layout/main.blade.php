31cb9e891970c098bf7f984d8e6d0e8e7e973b0a
arhizsx <arhizsx@gmail.com>
2025-02-28 20:43:40 +0800

    vouchers module

--- resources/views/modules/vouchers/layout/main.blade.php
+++ resources/views/modules/vouchers/layout/main.blade.php
@@ -74,7 +74,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+            @include('modules.vouchers.layout.navigation')
 
             <!-- Page Heading -->
             @isset($header)

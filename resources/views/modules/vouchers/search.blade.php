962f592000ff635bcfe4888f419a07b528999ab7
arhizsx <arhizsx@gmail.com>
2025-03-03 12:35:02 +0800

    search function

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -137,23 +137,23 @@ $("#search_form").on("submit", function(e) {
                                                 `</div>` +
                                             `</div>` +
 
-                                            `<div class='row'>` +
-                                                `<div class='col-2'>` +
+                                            `<div class='row mt-3'>` +
+                                                `<div class='col-2 border'>` +
                                                     item["Product SKU"] +
                                                 `</div>` +
-                                                `<div class='col-2'>` +
+                                                `<div class='col-2 border'>` +
                                                     item["Purchased At (Date+Time)"] +
                                                 `</div>` +
-                                                `<div class='col-2'>` +
+                                                `<div class='col-2 border'>` +
                                                     item["Payment Method"] +
                                                 `</div>` +
-                                                `<div class='col-2'>` +
+                                                `<div class='col-2 border'>` +
                                                     item["Payment Status"] +
                                                 `</div>` +
-                                                `<div class='col-2'>` +
+                                                `<div class='col-2 border'>` +
                                                     item["Payment Transaction ID"] +
                                                 `</div>` +
-                                                `<div class='col-2'>` +
+                                                `<div class='col-2 border'>` +
                                                     item["Revenue"] +
                                                 `</div>` +
                                             `</div>` +

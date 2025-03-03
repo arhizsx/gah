41c6babae522f5e189a831f938a9fc923fc4c2f5
arhizsx <arhizsx@gmail.com>
2025-03-03 08:25:43 +0800

    search function

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -26,9 +26,9 @@
                     <label class="mb-3">Mobile Number / Email / Name</label>
                     <div class="position-relative"> <!-- Container for input and X button -->
                         <input type="text" class="form-control w-100 mb-3" style="font-size: 2em;" name="search" id="search">
-                        <button type="button" id="clear_search" class="btn btn-link position-absolute end-0 top-50 translate-middle-y">
+                        <button type="button" id="clear_search" class="btn btn-link position-absolute end-0 top-50 translate-middle-y" style="display: none;">
                             <i class="fas fa-times"></i> <!-- Font Awesome X icon -->
-                        </button>                    
+                        </button>
                     </div>
                     <div class="d-flex justify-content-end">
                         <button type="button" class="btn btn-secondary rounded-3 px-5 me-3">Clear</button>

3c5b4d651febada6990208322440ba3411e14786
arhizsx <arhizsx@gmail.com>
2025-03-05 02:15:14 +0800

    Fix Uploading

--- app/Http/Controllers/SupervendorController.php
+++ app/Http/Controllers/SupervendorController.php
@@ -863,11 +863,8 @@ class SupervendorController extends Controller
                     // Close the stream after writing
                     // fclose($stream);
 
-
-                    return "https://storage.googleapis.com/" . env('GOOGLE_CLOUD_STORAGE_BUCKET') . "/" . $filePath;
-
                     // Save the public URL or file path
-                    $data[$f] = Storage::disk('gcs')->url($filePath);
+                    $data[$f] = $fileName;
 
                 }
             }

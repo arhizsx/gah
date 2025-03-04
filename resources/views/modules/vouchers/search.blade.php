cc5f44b3671ecf9a3c013a47da4778fe3fdf444c
arhizsx <arhizsx@gmail.com>
2025-03-05 03:37:50 +0800

    Fix Uploading

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -183,8 +183,7 @@ $("#search_form").on("submit", function(e) {
                                                     `<div class='search_info'>` + item["Redemption Date"] + `</div>`+
                                                 `</div>` +
                                                 `<div class='col-3 border'>` +
-                                                    `<div><small>Action</small></div>` +
-                                                    `<button class="btn btn-primary">RESEND VOUCHER</button>` +
+                                                    `<button class="btn btn-primary class="form-control"">RESEND VOUCHER</button>` +
                                                 `</div>` +
                                             `</div>` +
                                             `<div class='row mt-3' style='font-size: 0.8em'>` +

ddafffddc5e706f23b60c43404283773c7d84a43
arhizsx <arhizsx@gmail.com>
2025-03-05 11:14:07 +0800

    Fix Uploading

--- resources/views/modules/vouchers/layout/main.blade.php
+++ resources/views/modules/vouchers/layout/main.blade.php
@@ -87,7 +87,7 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
 
-            {{ $voucher_user }}
+            {{ $position }}
             @include('modules.vouchers.layout.navigation', [ "position" => $position])
 
             <!-- Page Heading -->

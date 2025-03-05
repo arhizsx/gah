c110085f0021181f9211afdfd58845e822592a9b
arhizsx <arhizsx@gmail.com>
2025-03-05 21:20:22 +0800

    Module Chooser

--- resources/views/components/module.blade.php
+++ resources/views/components/module.blade.php
@@ -1,3 +1,3 @@
-<div class="border shadow-lg bg-white px-5 py-3 rounded-5" style="max-width: 20vw;">
+<div class="border shadow-lg bg-white px-5 py-3 rounded-5" style="max-width: 30hw;">
     Vouchers
 </div>

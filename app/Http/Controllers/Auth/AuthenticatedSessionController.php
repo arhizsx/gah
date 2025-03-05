90e40c67ebc65abd45c67ae06082b59131669477
arhizsx <arhizsx@gmail.com>
2025-03-05 13:20:39 +0800

    Fix Uploading

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,8 +34,8 @@ class AuthenticatedSessionController extends Controller
 
         if( $modules ){
 
-            return $modules;
-            
+             dd($modules);
+
             if( count($modules) == 0  ){
 
                 return redirect()->intended(route('home', absolute: false));

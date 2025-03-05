4413cc6062ea090408537b240d2c239e0af8b857
arhizsx <arhizsx@gmail.com>
2025-03-05 22:00:45 +0800

    Module Chooser

--- resources/views/modules/system/chooser.blade.php
+++ resources/views/modules/system/chooser.blade.php
@@ -9,11 +9,11 @@
     <x-slot name="slot">
     <div class="container d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 30vh)">
 
-                @php 
-                    print_r( $modules[0]->label );
-                @endphp
 
         @foreach( $modules as $module )
+                @php 
+                    print_r( $module );
+                @endphp
             <x-module>
                 @php 
                     print_r( $module );

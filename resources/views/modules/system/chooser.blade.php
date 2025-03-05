e7f6bd9c68734335afc70bfcf98effff64555468
arhizsx <arhizsx@gmail.com>
2025-03-05 22:17:41 +0800

    Module Chooser

--- resources/views/modules/system/chooser.blade.php
+++ resources/views/modules/system/chooser.blade.php
@@ -11,7 +11,7 @@
 
         <div class="row">   
         @foreach( $modules as $module )
-            <div class="border shadow-lg bg-white px-5 py-3 rounded-5 col-auto">
+            <div class="border shadow-lg bg-white px-5 py-3 rounded-5 col" style="min-width: 400px;">
             {{ $module->label }}
             </div>
         @endforeach

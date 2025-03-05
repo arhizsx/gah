8204ccdcad71ffc70fcc60c6ba66e1e43763306e
arhizsx <arhizsx@gmail.com>
2025-03-05 22:31:05 +0800

    Module Chooser

--- resources/views/modules/system/chooser.blade.php
+++ resources/views/modules/system/chooser.blade.php
@@ -12,7 +12,7 @@
         <div class="row">   
         @foreach( $modules as $module )
             <div class="border text-center shadow-lg bg-white px-5 py-3 rounded-5 col" style="min-width: 400px;">
-                <div style="height: 300px;"> <img src="{{ $module->image }}" /> </div>
+                <div> <img width="100%" src="{{ $module->image }}" /> </div>
                 <div style="font-size: 1.25em; font-weight: bolder;"> {{ $module->label }} </div>
             </div>
         @endforeach

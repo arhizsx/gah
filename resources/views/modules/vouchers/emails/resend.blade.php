8b76dbd812a4f2e27357f04ff9b83d86ae8e8e47
arhizsx <arhizsx@gmail.com>
2025-03-06 23:16:50 +0800

    Resend Voucher

--- resources/views/modules/vouchers/emails/resend.blade.php
+++ resources/views/modules/vouchers/emails/resend.blade.php
@@ -1,31 +1,63 @@
-<div id=":96" class="ii gt" jslog="20277; u014N:xr6bB; 1:WyIjdGhyZWFkLWY6MTgyNTI4NzU4ODc0OTQyMDIyMiJd; 4:WyIjbXNnLWY6MTgyNTI4NzU4ODc0OTQyMDIyMiIsbnVsbCxudWxsLG51bGwsMSwwLFsxLDAsMF0sMTI4LDc5NyxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsMSxudWxsLG51bGwsWzNdLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLDBd"><div id=":95" class="a3s aiL "><div class="adM">
-      </div><div style="width:600px;margin:0 auto;text-align:left"><div class="adM">
-    
-        </div><img src="https://ci3.googleusercontent.com/meips/ADKq_NYBhNucr67NVwIgzyK6Cwhgp9g8Blxs9Re7Z5_q2BjBg1tfUL45makEddXZqHqKFoHlL1UgiQ=s0-d-e1-ft#https://i.imgur.com/oXPdHrA.jpeg" alt="Header Image" width="600" height="100" style="display:block" class="CToWUd a6T" data-bit="iit" tabindex="0"><div class="a6S" dir="ltr" style="opacity: 0.01; left: 588px; top: 64px;"><span data-is-tooltip-wrapper="true" class="a5q" jsaction="JIbuQc:.CLIENT"><button class="VYBDae-JX-I VYBDae-JX-I-ql-ay5-ays CgzRE" jscontroller="PIVayb" jsaction="click:h5M12e; clickmod:h5M12e;pointerdown:FEiYhc;pointerup:mF5Elf;pointerenter:EX0mI;pointerleave:vpvbp;pointercancel:xyn4sd;contextmenu:xexox;focus:h06R8; blur:zjh6rb;mlnRJb:fLiPzd;" data-idom-class="CgzRE" data-use-native-focus-logic="true" jsname="hRZeKc" aria-label="Download attachment " data-tooltip-enabled="true" data-tooltip-id="tt-c34" data-tooltip-classes="AZPksf" id="" jslog="91252; u014N:cOuCgd,Kr2w4b,xr6bB; 4:WyIjbXNnLWY6MTgyNTI4NzU4ODc0OTQyMDIyMiJd; 43:WyJpbWFnZS9qcGVnIl0."><span class="OiePBf-zPjgPe VYBDae-JX-UHGRz"></span><span class="bHC-Q" jscontroller="LBaJxb" jsname="m9ZlFb" soy-skip="" ssk="6:RWVI5c"></span><span class="VYBDae-JX-ank-Rtc0Jf" jsname="S5tZuc" aria-hidden="true"><span class="notranslate bzc-ank" aria-hidden="true"><svg viewBox="0 -960 960 960" height="20" width="20" focusable="false" class=" aoH"><path d="M480-336L288-528l51-51L444-474V-816h72v342L621-579l51,51L480-336ZM263.72-192Q234-192 213-213.15T192-264v-72h72v72H696v-72h72v72q0,29.7-21.16,50.85T695.96-192H263.72Z"></path></svg></span></span><div class="VYBDae-JX-ano"></div></button><div class="ne2Ple-oshW8e-J9" id="tt-c34" role="tooltip" aria-hidden="true">Download</div></span></div>
+<div id=":96" class="ii gt" jslog=""><div id=":95" class="a3s aiL ">
+    <div style="width:600px;margin:0 auto;text-align:left">
+        <img src="https://ci3.googleusercontent.com/meips/ADKq_NYBhNucr67NVwIgzyK6Cwhgp9g8Blxs9Re7Z5_q2BjBg1tfUL45makEddXZqHqKFoHlL1UgiQ=s0-d-e1-ft#https://i.imgur.com/oXPdHrA.jpeg" alt="Header Image" width="600" height="100" style="display:block" class="CToWUd a6T" data-bit="iit" tabindex="0">
         <br>
-      
-      <div style="width:570px;padding:0 10px 0 20px;font-family:Arial,sans-serif;line-height:1.5;word-wrap:break-word">
-        Hi Lorelei! <br><br>
-Ang iyong GFiber Prepaid promo activation noong <strong>Feb 26, 2025 12:06 AM</strong> ay may kasamang <strong>1-month Disney+ Premium access!</strong> <br><br>
+        <div style="width:570px;padding:0 10px 0 20px;font-family:Arial,sans-serif;line-height:1.5;word-wrap:break-word">
+            Hi Lorelei! 
+            <br><br>
+            Ang iyong GFiber Prepaid promo activation noong <strong>Feb 26, 2025 12:06 AM</strong> ay may kasamang <strong>1-month Disney+ Premium access!</strong> 
+            <br><br>
 
-<img data-emoji="🎉" class="an1" alt="🎉" aria-label="🎉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f389/72.png" loading="lazy"> Gamitin ang code na pinadala sa iyong registered GFiber Prepaid number <br>
-<img data-emoji="🔗" class="an1" alt="🔗" aria-label="🔗" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f517/72.png" loading="lazy"> <strong>I-redeem sa Disney+ website</strong> para mapanood ang iyong mga paboritong shows! <br>
-<img data-emoji="👉" class="an1" alt="👉" aria-label="👉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f449/72.png" loading="lazy"> <strong>Paano i-redeem?</strong> Tingnan dito: <a href="https://www.facebook.com/share/v/1X7x16JJL1/" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.facebook.com/share/v/1X7x16JJL1/&amp;source=gmail&amp;ust=1741358351767000&amp;usg=AOvVaw0iNMowWfX9wtra4l9gDoCQ">https://www.facebook.com/<wbr>share/v/1X7x16JJL1/</a> <br><br>
+            <img data-emoji="🎉" class="an1" alt="🎉" aria-label="🎉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f389/72.png" loading="lazy"> Gamitin ang code na pinadala sa iyong registered GFiber Prepaid number 
+            <br>
+            <img data-emoji="🔗" class="an1" alt="🔗" aria-label="🔗" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f517/72.png" loading="lazy"> <strong>I-redeem sa Disney+ website</strong> para mapanood ang iyong mga paboritong shows! 
+            <br>
+            <img data-emoji="👉" class="an1" alt="👉" aria-label="👉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f449/72.png" loading="lazy"> <strong>Paano i-redeem?</strong> Tingnan dito: <a href="https://www.facebook.com/share/v/1X7x16JJL1/" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.facebook.com/share/v/1X7x16JJL1/&amp;source=gmail&amp;ust=1741358351767000&amp;usg=AOvVaw0iNMowWfX9wtra4l9gDoCQ">https://www.facebook.com/<wbr>share/v/1X7x16JJL1/</a> 
+            <br><br>
+            <img data-emoji="⚠" class="an1" alt="⚠" aria-label="⚠" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/26a0/72.png" loading="lazy"> <strong>Tandaan:</strong> 
+            <br>
+            <img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Huwag i-share ang iyong code. Ito ay maaari lamang gamitin ng <strong>isang beses</strong>. 
+            <br>
+            <img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> I-activate sa loob ng <strong>30 days</strong> mula sa pagtanggap ng mensaheng ito. 
+            <br>
+            <img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Hindi magagamit ang code kung may existing Disney+ subscription ka. 
+            <br>
+            <img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Pwede kang gumamit ng ibang Disney+ account para ma-redeem ito. 
+            <br><br>
 
-<img data-emoji="⚠" class="an1" alt="⚠" aria-label="⚠" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/26a0/72.png" loading="lazy"> <strong>Tandaan:</strong> <br>
-<img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Huwag i-share ang iyong code. Ito ay maaari lamang gamitin ng <strong>isang beses</strong>. <br>
-<img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> I-activate sa loob ng <strong>30 days</strong> mula sa pagtanggap ng mensaheng ito. <br>
-<img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Hindi magagamit ang code kung may existing Disney+ subscription ka. <br>
-<img data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Pwede kang gumamit ng ibang Disney+ account para ma-redeem ito. <br><br>
-
-Enjoy your Disney+ Premium access! <img data-emoji="🍿" class="an1" alt="🍿" aria-label="🍿" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f37f/72.png" loading="lazy"><img data-emoji="🎬" class="an1" alt="🎬" aria-label="🎬" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f3ac/72.png" loading="lazy"> <br><br>
-<span style="font-size:x-small;font-style:italic">This is a system-generated message and does not require a signature. Please do not reply to this email.</span><br><br>
-      </div>
-    
+            Enjoy your Disney+ Premium access! <img data-emoji="🍿" class="an1" alt="🍿" aria-label="🍿" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f37f/72.png" loading="lazy"><img data-emoji="🎬" class="an1" alt="🎬" aria-label="🎬" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f3ac/72.png" loading="lazy"> 
+            <br><br>
+            <span style="font-size:x-small;font-style:italic">
+                This is a system-generated message and does not require a signature. Please do not reply to this email.
+            </span>
+            <br><br>
+        </div>
         <br>
-        <img src="https://ci3.googleusercontent.com/meips/ADKq_NZxTh2pBCj93PoQQ3ehwTZc6aFHv-afu92_x0_RD1uLeU0TcbvQ07aSOGE-74Y_GmkofMLRcQ=s0-d-e1-ft#https://i.imgur.com/AjaKUim.jpeg" alt="Embedded Image" width="600" height="100" style="display:block" class="CToWUd a6T" data-bit="iit" tabindex="0"><div class="a6S" dir="ltr" style="opacity: 0.01; left: 588px; top: 573.5px;"><span data-is-tooltip-wrapper="true" class="a5q" jsaction="JIbuQc:.CLIENT"><button class="VYBDae-JX-I VYBDae-JX-I-ql-ay5-ays CgzRE" jscontroller="PIVayb" jsaction="click:h5M12e; clickmod:h5M12e;pointerdown:FEiYhc;pointerup:mF5Elf;pointerenter:EX0mI;pointerleave:vpvbp;pointercancel:xyn4sd;contextmenu:xexox;focus:h06R8; blur:zjh6rb;mlnRJb:fLiPzd;" data-idom-class="CgzRE" data-use-native-focus-logic="true" jsname="hRZeKc" aria-label="Download attachment " data-tooltip-enabled="true" data-tooltip-id="tt-c35" data-tooltip-classes="AZPksf" id="" jslog="91252; u014N:cOuCgd,Kr2w4b,xr6bB; 4:WyIjbXNnLWY6MTgyNTI4NzU4ODc0OTQyMDIyMiJd; 43:WyJpbWFnZS9qcGVnIl0."><span class="OiePBf-zPjgPe VYBDae-JX-UHGRz"></span><span class="bHC-Q" jscontroller="LBaJxb" jsname="m9ZlFb" soy-skip="" ssk="6:RWVI5c"></span><span class="VYBDae-JX-ank-Rtc0Jf" jsname="S5tZuc" aria-hidden="true"><span class="notranslate bzc-ank" aria-hidden="true"><svg viewBox="0 -960 960 960" height="20" width="20" focusable="false" class=" aoH"><path d="M480-336L288-528l51-51L444-474V-816h72v342L621-579l51,51L480-336ZM263.72-192Q234-192 213-213.15T192-264v-72h72v72H696v-72h72v72q0,29.7-21.16,50.85T695.96-192H263.72Z"></path></svg></span></span><div class="VYBDae-JX-ano"></div></button><div class="ne2Ple-oshW8e-J9" id="tt-c35" role="tooltip" aria-hidden="true">Download</div></span></div>
-      
+        <img src="https://ci3.googleusercontent.com/meips/ADKq_NZxTh2pBCj93PoQQ3ehwTZc6aFHv-afu92_x0_RD1uLeU0TcbvQ07aSOGE-74Y_GmkofMLRcQ=s0-d-e1-ft#https://i.imgur.com/AjaKUim.jpeg" alt="Embedded Image" width="600" height="100" style="display:block" class="CToWUd a6T" data-bit="iit" tabindex="0">
+        <div class="a6S" dir="ltr" style="opacity: 0.01; left: 588px; top: 573.5px;">
+            <span data-is-tooltip-wrapper="true" class="a5q" jsaction="JIbuQc:.CLIENT">
+                <button class="VYBDae-JX-I VYBDae-JX-I-ql-ay5-ays CgzRE" jscontroller="PIVayb" jsaction="click:h5M12e; clickmod:h5M12e;pointerdown:FEiYhc;pointerup:mF5Elf;pointerenter:EX0mI;pointerleave:vpvbp;pointercancel:xyn4sd;contextmenu:xexox;focus:h06R8; blur:zjh6rb;mlnRJb:fLiPzd;" data-idom-class="CgzRE" data-use-native-focus-logic="true" jsname="hRZeKc" aria-label="Download attachment " data-tooltip-enabled="true" data-tooltip-id="tt-c35" data-tooltip-classes="AZPksf" id="" jslog="91252; u014N:cOuCgd,Kr2w4b,xr6bB; 4:WyIjbXNnLWY6MTgyNTI4NzU4ODc0OTQyMDIyMiJd; 43:WyJpbWFnZS9qcGVnIl0.">
+                    <span class="OiePBf-zPjgPe VYBDae-JX-UHGRz">
+
+                    </span>
+                    <span class="bHC-Q" jscontroller="LBaJxb" jsname="m9ZlFb" soy-skip="" ssk="6:RWVI5c">
+
+                    </span>
+                    <span class="VYBDae-JX-ank-Rtc0Jf" jsname="S5tZuc" aria-hidden="true">
+                        <span class="notranslate bzc-ank" aria-hidden="true">
+                            <svg viewBox="0 -960 960 960" height="20" width="20" focusable="false" class=" aoH"><path d="M480-336L288-528l51-51L444-474V-816h72v342L621-579l51,51L480-336ZM263.72-192Q234-192 213-213.15T192-264v-72h72v72H696v-72h72v72q0,29.7-21.16,50.85T695.96-192H263.72Z"></path></svg>
+                        </span>
+                    </span>
+                    <div class="VYBDae-JX-ano">
+
+                    </div>
+                </button>
+                <div class="ne2Ple-oshW8e-J9" id="tt-c35" role="tooltip" aria-hidden="true">
+                    Download
+                </div>
+            </span>
+        </div>
         <br>
-        <img src="https://ci3.googleusercontent.com/meips/ADKq_NbIDTJ0GezpyJYXhxjAu0F_mQtkbJOUezyAgL5-ykMtSA8NAM9Fh-oaJ3WpRwa4LTV_iGuB=s0-d-e1-ft#https://i.imgur.com/M9tg07E.png" alt="Disclaimer Image" width="600" height="100" style="display:block" class="CToWUd a6T" data-bit="iit" tabindex="0"><div class="a6S" dir="ltr" style="opacity: 0.01; left: 588px; top: 693px;"><span data-is-tooltip-wrapper="true" class="a5q" jsaction="JIbuQc:.CLIENT"><button class="VYBDae-JX-I VYBDae-JX-I-ql-ay5-ays CgzRE" jscontroller="PIVayb" jsaction="click:h5M12e; clickmod:h5M12e;pointerdown:FEiYhc;pointerup:mF5Elf;pointerenter:EX0mI;pointerleave:vpvbp;pointercancel:xyn4sd;contextmenu:xexox;focus:h06R8; blur:zjh6rb;mlnRJb:fLiPzd;" data-idom-class="CgzRE" data-use-native-focus-logic="true" jsname="hRZeKc" aria-label="Download attachment " data-tooltip-enabled="true" data-tooltip-id="tt-c36" data-tooltip-classes="AZPksf" id="" jslog="91252; u014N:cOuCgd,Kr2w4b,xr6bB; 4:WyIjbXNnLWY6MTgyNTI4NzU4ODc0OTQyMDIyMiJd; 43:WyJpbWFnZS9qcGVnIl0."><span class="OiePBf-zPjgPe VYBDae-JX-UHGRz"></span><span class="bHC-Q" jscontroller="LBaJxb" jsname="m9ZlFb" soy-skip="" ssk="6:RWVI5c"></span><span class="VYBDae-JX-ank-Rtc0Jf" jsname="S5tZuc" aria-hidden="true"><span class="notranslate bzc-ank" aria-hidden="true"><svg viewBox="0 -960 960 960" height="20" width="20" focusable="false" class=" aoH"><path d="M480-336L288-528l51-51L444-474V-816h72v342L621-579l51,51L480-336ZM263.72-192Q234-192 213-213.15T192-264v-72h72v72H696v-72h72v72q0,29.7-21.16,50.85T695.96-192H263.72Z"></path></svg></span></span><div class="VYBDae-JX-ano"></div></button><div class="ne2Ple-oshW8e-J9" id="tt-c36" role="tooltip" aria-hidden="true">Download</div></span></div><div class="yj6qo"></div><div class="adL">
-      </div></div><div class="adL">
-</div></div></div>
+        <img src="https://ci3.googleusercontent.com/meips/ADKq_NbIDTJ0GezpyJYXhxjAu0F_mQtkbJOUezyAgL5-ykMtSA8NAM9Fh-oaJ3WpRwa4LTV_iGuB=s0-d-e1-ft#https://i.imgur.com/M9tg07E.png" alt="Disclaimer Image" width="600" height="100" style="display:block" class="CToWUd a6T" data-bit="iit" tabindex="0"><div class="a6S" dir="ltr" style="opacity: 0.01; left: 588px; top: 693px;">
+    </div>
+</div>

866f254fd1c80804e2cca378dc619fe3c6e3ef57
arhizsx <arhizsx@gmail.com>
2025-03-06 19:52:39 +0800

    Resend Voucher

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -44,7 +44,7 @@
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="serviceModallLabel">Resend Voucher</h5>
+                    <h1 class="modal-title" id="serviceModallLabel">Resend Voucher</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -52,7 +52,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary serviceButton" data-action="confirmResendVoucher" data-id="">Send</button>
                 </div>
                 </div>
             </div>
@@ -66,8 +66,7 @@ $("#results_box").on("click", ".serviceButton", function(e) {
 
     e.preventDefault();
     
-    console.log("Service Button");
-    console.log($(document).find("#serviceModal").length); 
+    console.log($(document).find("#serviceModal").data()); 
 
 
     $("#serviceModal").modal("show");

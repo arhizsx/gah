4a396383c1ddb5202507f044d8f11e5d08356c30
arhizsx <arhizsx@gmail.com>
2025-03-06 23:24:48 +0800

    Resend Voucher

--- resources/views/modules/vouchers/emails/resend.blade.php
+++ resources/views/modules/vouchers/emails/resend.blade.php
@@ -14,24 +14,24 @@
             Ang iyong GFiber Prepaid promo activation noong <strong>Feb 26, 2025 12:06 AM</strong> ay may kasamang <strong>1-month Disney+ Premium access!</strong> 
             <br><br>
 
-            <img style="width: 15px;" data-emoji="🎉" class="an1" alt="🎉" aria-label="🎉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f389/72.png" loading="lazy"> Gamitin ang code na pinadala sa iyong registered GFiber Prepaid number 
+            <img style="width: 20px;" data-emoji="🎉" class="an1" alt="🎉" aria-label="🎉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f389/72.png" loading="lazy"> Gamitin ang code na pinadala sa iyong registered GFiber Prepaid number 
             <br>
-            <img style="width: 15px;" data-emoji="🔗" class="an1" alt="🔗" aria-label="🔗" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f517/72.png" loading="lazy"> <strong>I-redeem sa Disney+ website</strong> para mapanood ang iyong mga paboritong shows! 
+            <img style="width: 20px;" data-emoji="🔗" class="an1" alt="🔗" aria-label="🔗" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f517/72.png" loading="lazy"> <strong>I-redeem sa Disney+ website</strong> para mapanood ang iyong mga paboritong shows! 
             <br>
-            <img style="width: 15px;" data-emoji="👉" class="an1" alt="👉" aria-label="👉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f449/72.png" loading="lazy"> <strong>Paano i-redeem?</strong> Tingnan dito: <a href="https://www.facebook.com/share/v/1X7x16JJL1/" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.facebook.com/share/v/1X7x16JJL1/&amp;source=gmail&amp;ust=1741358351767000&amp;usg=AOvVaw0iNMowWfX9wtra4l9gDoCQ">https://www.facebook.com/<wbr>share/v/1X7x16JJL1/</a> 
+            <img style="width: 20px;" data-emoji="👉" class="an1" alt="👉" aria-label="👉" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f449/72.png" loading="lazy"> <strong>Paano i-redeem?</strong> Tingnan dito: <a href="https://www.facebook.com/share/v/1X7x16JJL1/" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.facebook.com/share/v/1X7x16JJL1/&amp;source=gmail&amp;ust=1741358351767000&amp;usg=AOvVaw0iNMowWfX9wtra4l9gDoCQ">https://www.facebook.com/<wbr>share/v/1X7x16JJL1/</a> 
             <br><br>
-            <img style="width: 15px;" data-emoji="⚠" class="an1" alt="⚠" aria-label="⚠" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/26a0/72.png" loading="lazy"> <strong>Tandaan:</strong> 
+            <img style="width: 20px;" data-emoji="⚠" class="an1" alt="⚠" aria-label="⚠" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/26a0/72.png" loading="lazy"> <strong>Tandaan:</strong> 
             <br>
-            <img style="width: 15px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Huwag i-share ang iyong code. Ito ay maaari lamang gamitin ng <strong>isang beses</strong>. 
+            <img style="width: 20px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Huwag i-share ang iyong code. Ito ay maaari lamang gamitin ng <strong>isang beses</strong>. 
             <br>
-            <img style="width: 15px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> I-activate sa loob ng <strong>30 days</strong> mula sa pagtanggap ng mensaheng ito. 
+            <img style="width: 20px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> I-activate sa loob ng <strong>30 days</strong> mula sa pagtanggap ng mensaheng ito. 
             <br>
-            <img style="width: 15px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Hindi magagamit ang code kung may existing Disney+ subscription ka. 
+            <img style="width: 20px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Hindi magagamit ang code kung may existing Disney+ subscription ka. 
             <br>
-            <img style="width: 15px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Pwede kang gumamit ng ibang Disney+ account para ma-redeem ito. 
+            <img style="width: 20px;" data-emoji="✔" class="an1" alt="✔" aria-label="✔" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/2714/72.png" loading="lazy"> Pwede kang gumamit ng ibang Disney+ account para ma-redeem ito. 
             <br><br>
 
-            Enjoy your Disney+ Premium access! <img  style="width: 15px;" data-emoji="🍿" class="an1" alt="🍿" aria-label="🍿" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f37f/72.png" loading="lazy"><img  style="width: 15px;" data-emoji="🎬" class="an1" alt="🎬" aria-label="🎬" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f3ac/72.png" loading="lazy"> 
+            Enjoy your Disney+ Premium access! <img  style="width: 20px;" data-emoji="🍿" class="an1" alt="🍿" aria-label="🍿" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f37f/72.png" loading="lazy"><img  style="width: 20px;" data-emoji="🎬" class="an1" alt="🎬" aria-label="🎬" draggable="false" src="https://fonts.gstatic.com/s/e/notoemoji/16.0/1f3ac/72.png" loading="lazy"> 
             <br><br>
             <span style="font-size:x-small;font-style:italic">
                 This is a system-generated message and does not require a signature. Please do not reply to this email.

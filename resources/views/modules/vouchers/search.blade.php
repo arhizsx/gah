befeee357b8967c7e07652674b4fe0d3a028c2b8
arhizsx <arhizsx@gmail.com>
2025-03-06 19:45:09 +0800

    Resend Voucher

--- resources/views/modules/vouchers/search.blade.php
+++ resources/views/modules/vouchers/search.blade.php
@@ -41,10 +41,10 @@
         </div>
         <!-- Modal -->
         <div class="modal fade" id="serviceModal" tabindex="-1" aria-labelledby="serviceModallLabel" aria-hidden="false">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-xl">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="serviceModallLabel">Modal title</h5>
+                    <h5 class="modal-title" id="serviceModallLabel">Resend Voucher</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">

e444e37a6f23b77fa26cb54f729278f56a0f47ee
arhizsx <arhizsx@gmail.com>
2025-03-07 19:19:39 +0800

    module Checker

--- resources/views/modules/system/chooser.blade.php
+++ resources/views/modules/system/chooser.blade.php
@@ -10,8 +10,30 @@
     <div class="container justify-content-center align-items-center px-3 d-flex" style="min-height: calc(100vh - 10vh)">
 
         <div class="row">   
+        @php  
+
+          $counter =  count( $modules );
+
+          if( $counter == 1 ){
+            $col = 12;
+          }
+          elseif( $counter == 2 ){
+            $col = 6;
+          }
+          elseif( $counter == 3 ){
+            $col = 4;
+          }
+          elseif( $counter == 4 ){
+            $col = 3;
+          } 
+          else {
+            $col = 3;
+          }
+
+        @endphp 
+
         @foreach( $modules as $module )
-            <a href="{{ $module->link }}" class="col-xl-3 col-lg-3 col-sm-6 col-xs-12">
+            <a href="{{ $module->link }}" class="col-xl-{{ $col }}">
                 <div class="border m-2 text-center shadow-lg bg-white  p-3 rounded-5">
                     <div> <img width="100%" src="{{ $module->image }}" class="rounded-5" /> </div>
                     <div style="font-size: 1.25em; font-weight: bolder;"> {{ $module->label }} </div>
